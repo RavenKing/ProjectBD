056ec9d0693a87e5905b51e3237a7ee4e7617acc
Cassie1111 <495550641@qq.com>
2016-01-15 14:40:05 +0800

    Initial version
    Initial version

--- WebContent/competitoranalysis/Analysis.view.js
+++ WebContent/competitoranalysis/Analysis.view.js
@@ -13,74 +13,123 @@ sap.ui.jsview("competitoranalysis.Analysis", {
 	* @memberOf competitoranalysis.Analysis
 	*/ 
 	createContent : function(oController) {
+		var oMatrix = new sap.ui.commons.layout.MatrixLayout("searchMatrixId",{
+			width:"700px",
+			column:5
+		}).addStyleClass("matrixStyle");
+		oMatrix.createRow(
+				
+				new sap.ui.commons.Label({text:"Company"}).addStyleClass("CompanyStyle"),
+				new sap.ui.commons.Label({text:"Field"}).addStyleClass("FieldStyle"),				
+				new sap.ui.commons.Label({text:"Time"}).addStyleClass("TimeStyle"),
+				new sap.ui.commons.Label({text:"Orderby"}).addStyleClass("OrderbyStyle"),
+				new sap.ui.commons.Label({text:"Source"}).addStyleClass("SourceStyle")
+				
+				
+		);
 		
-		var oMatrix = new sap.ui.commons.layout.MatrixLayout(this.createId("searchMatrixId"));
 		oMatrix.createRow(
-				new sap.ui.commons.Label({text:"Company"}),
-				new sap.ui.commons.Label({text:"Field"}),
-				new sap.ui.commons.Label({text:"Keyword"}),
-				new sap.ui.commons.Label({text:"Time"}),
-				new sap.ui.commons.Label({text:"Orderby"}),
-				new sap.ui.commons.Label({text:"Source"})
+				
+				new sap.ui.commons.DropdownBox(this.createId("companyBoxId")).addStyleClass("companyBoxStyle"),
+				new sap.ui.commons.DropdownBox(this.createId("fieldBoxId")).addStyleClass("fieldBoxStyle"),				
+				new sap.ui.commons.DropdownBox(this.createId("timeBoxId")).addStyleClass("timeBoxStyle"),
+				new sap.ui.commons.DropdownBox(this.createId("orderbyBoxId")).addStyleClass("orderbyBoxStyle"),
+				new sap.ui.commons.DropdownBox(this.createId("sourceBoxId")).addStyleClass("sourceBoxStyle")
+		
 		);
-		oMatrix.createRow(
-				new sap.ui.commons.DropdownBox(this.createId("companyBoxId")),
-				new sap.ui.commons.DropdownBox(this.createId("fieldBoxId")),
-				new sap.ui.commons.TextField(this.createId("keywordFieldId")),
-				new sap.ui.commons.DropdownBox(this.createId("timeBoxId")),
-				new sap.ui.commons.DropdownBox(this.createId("orderbyBoxId")),
-				new sap.ui.commons.DropdownBox(this.createId("sourceBoxId")),
-				new sap.ui.commons.Button(this.createId("searchBtnId"),{
-					text:"Search",
-					width:"100px",
-					press: function(){
-						oController.search();
-					}
-				})
-		);
+		
 		var oPanel1 = new sap.ui.commons.Panel("panel1Id",{
-			text:"panel1",
+			text:" Related news",
 			width:"100%",
-			height:"20%",
-			content:[
-
+			height:"20%"
 			
-			]
 			
 		});
 		var oPanel2 = new sap.ui.commons.Panel(this.createId("panel2Id"),{
-			text:"panel2",
-			width:"50%",
-			height:"35%",
+			text:" Visual graphs",
+			width:"62%",
+			height:"500px",
 			content:[
-			new sap.viz.ui5.controls.VizFrame({  
-					    	id : this.createId("PieChart"),
-					    	width : "100%",
-					    	height : "100%"
-					    })	
+			         new sap.viz.ui5.controls.VizFrame({  
+					    	id : "CompanyPieChart",
+					    	title:"Company",
+					    	width : "50%",
+					    	height : "50%"
+					    }).addStyleClass("ChartStyle"),
+					  new sap.viz.ui5.controls.VizFrame({  
+					    	id : "FieldPieChart",
+					    	title:"Field",
+					    	width : "50%",
+					    	height : "50%"
+					    }).addStyleClass("ChartStyle"),
+					  new sap.viz.ui5.controls.VizFrame({  
+					    	id : "TimeLineChart",
+					    	title:"Time",
+					    	width : "50%",
+					    	height : "50%"
+					    }).addStyleClass("ChartStyle"),
+					  new sap.viz.ui5.controls.VizFrame({  
+					    	id : "SourcePieChart",
+					    	title:"Source",
+					    	width : "50%",
+					    	height : "50%"
+					   }).addStyleClass("ChartStyle")	
 			
 			
 			
 			
 			]
-		});
+		}).addStyleClass("Panel2StyleClass");
 		var oPanel3 = new sap.ui.commons.Panel("panel3Id",{
-			text:"panel3",
-			width:"50%",
-			height:"35%"
-		});
+			text:"Frequent words",
+			width:"38%",
+			height:"500px",
+			content:[
+			    
+			    new sap.m.TextArea({
+			    	width:"100%",
+			    	height:"100%",
+			    	value:"热词显示图以及与热词相关的新闻"
+			    })
+			    
+			]
+		}).addStyleClass("Panel3StyleClass");
 		var oPanel4 = new sap.ui.commons.Panel("panel4Id",{
-			text:"panel4",
-			width:"80%",
-			height:"30%"
+			text:" Analytical contents",
+			width:"100%",
+			height:"30%",
+			content:[
+					    
+					    new sap.m.TextArea({
+					    	width:"100%",
+					    	height:"100%",
+					    	value:"分析文本：例如\n" +
+					    			"2015年期间，IBM在医疗领域备受关注，其中点击量前三的新闻有http://###########"
+					    })
+					    
+		    ]
 			
 		});
 		
 		
 		var oPage = new sap.m.Page({
-			title:"Competitor Analysis",
+			showHeader:false,			
 			content:[
-			   oMatrix,
+			  
+			  oMatrix,
+			   
+			   new sap.ui.commons.TextField(this.createId("searchInfoFieldId"),{
+				   width:"700px",
+				   change:function(){
+					   oController.search();
+				   }
+			   }).addStyleClass("keywordstyle"),
+			   new sap.ui.commons.Button(this.createId("searchBtnId"),{
+					text:"Search",
+					press: function(){
+						oController.search();
+					}
+				}).addStyleClass("SearchBtn"),
 			   oPanel1,
 			   oPanel2,
 			   oPanel3,
